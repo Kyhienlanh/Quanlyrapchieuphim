a911e777b0a4367b2b5a7d50291da0ac512751b3
kyhienlanh@gmail.com <kyhienlanh@gmail.com>
2024-02-24 08:14:15 +0700

    Add project files.

---
+++ Quanlyrapchieuphim/Controllers/BaoCaoThongKeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Quanlyrapchieuphim.Controllers
+{
+    public class BaoCaoThongKeController : Controller
+    {
+        // GET: BaoCaoThongKe
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Quanlyrapchieuphim/Controllers/ChitietphimController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Quanlyrapchieuphim.Controllers
+{
+    public class ChitietphimController : Controller
+    {
+        // GET: Chitietphim
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
